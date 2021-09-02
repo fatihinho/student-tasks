8032d5896dbfeeb00e46332f182eccc98cc69f51
Fatih Çınar <fcinar_38@hotmail.com>
2021-09-02 16:43:56 +0300

    Initial commit

---
+++ src/main/java/com/fcinar/studenttasks/StudentTasksApplication.java
@@ -0,0 +1,13 @@
+package com.fcinar.studenttasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcinar/studenttasks/StudentTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcinar.studenttasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
